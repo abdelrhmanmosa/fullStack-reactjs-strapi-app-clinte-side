4fda25718054de6052cf4bb1b2a79f71035d75eb
abdelrhmanmosa <abdelrhmanmosa94@gmail.com>
2024-06-26 00:58:33 +0300

    link project with strapi-app

--- src/api/axios.config.js
+++ src/api/axios.config.js
@@ -1,5 +1,15 @@
+// import axios from "axios";
+
+// export const axiosInstant = axios.create({
+//   baseURL: "http://localhost:1337",
+// });
+
 import axios from "axios";
 
-export const axiosInstant = axios.create({
-  baseURL: "http://localhost:1337",
+const apiUrl = "https://fullstack-reactjs-strapi-app-ael0.onrender.com/admin/"; // استبدل 'https://your-strapi-app.render.com' بعنوان Strapi المنشور
+
+const axiosInstance = axios.create({
+  baseURL: apiUrl,
 });
+
+export default axiosInstance;
